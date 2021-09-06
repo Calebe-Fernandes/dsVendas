4234de052bfd2375bee6b61f936ff462857ed239
Calebe-Fernandes <calebefernandesps4@gmail.com>
2021-09-06 11:11:08 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsVendas/DsVendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsVendas/DsVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
