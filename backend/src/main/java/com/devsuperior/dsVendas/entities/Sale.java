ff0766546c327483331f78b0e7b9c06a7ec31065
CalebeFernandes <calebefernandesps4@gmail.com>
2021-09-08 20:07:54 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsVendas/entities/Sale.java
@@ -0,0 +1,88 @@
+package com.devsuperior.dsVendas.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
+public class Sale {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Integer deals;
+	private Integer visited;
+	private Double amount;
+	private LocalDate date;
+	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
+	private Seller seller;
+	
+	public Sale() {
+	}
+
+	public Sale(Long id, Integer deals, Integer visited, Double amount, LocalDate date, Seller seller) {
+		this.id = id;
+		this.deals = deals;
+		this.visited = visited;
+		this.amount = amount;
+		this.date = date;
+		this.seller = seller;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public Seller getSeller() {
+		return seller;
+	}
+
+	public void setSeller(Seller seller) {
+		this.seller = seller;
+	}
+}
