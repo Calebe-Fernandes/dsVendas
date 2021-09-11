35926e27d9d6827021b1fbb84d0c72a3f9d4e735
CalebeFernandes <calebefernandesps4@gmail.com>
2021-09-10 22:45:45 -0300

    First request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,21 +1,46 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import {SaleSum} from 'types/sale'
+import { BASE_URL } from 'utils/requests';
+
+type ChartData = {
+    labels: string[];
+    series: number[];
+}
 
 const DonutChart = () => {
+
+    let chartData : ChartData = {labels: [], series: []};
+
+    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response => {
+
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.soma);
+
+            chartData = {
+                labels: myLabels,
+                series: mySeries
+            }
+            console.log(response.data);
+        });
+
     const mockData = {
         series: [477138, 499928, 444867, 220426],
         labels: ['Calebe', 'Luanda', 'Santiago', 'Valadares']
-    }
+    };
     
     const options = {
         legend: {
             show: true
         }
-    }
+    };
     
     return (
         <Chart
-            options = {{ ...options, labels: mockData.labels }}
-            series = {mockData.series}
+            options = {{ ...options, labels: chartData.labels }}
+            series = {chartData.series}
             type = "donut"
             height = "240"
         />
